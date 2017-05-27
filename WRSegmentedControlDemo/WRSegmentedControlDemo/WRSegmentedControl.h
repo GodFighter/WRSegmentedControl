967bbf8f368f2bd009f1a4074302740fda25a3d8
xianghui <xianghui_ios@163.com>
2017-05-27 10:11:17 +0800

    提交文件

---
+++ WRSegmentedControlDemo/WRSegmentedControlDemo/WRSegmentedControl.h
@@ -0,0 +1,13 @@
+//
+//  WRSegmentedControl.h
+//  WRSegmentedControlDemo
+//
+//  Created by xianghui on 2017/5/27.
+//  Copyright © 2017年 xianghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WRSegmentedControl : UIView
+
+@end
---
+++ WRSegmentedControlDemo/WRSegmentedControlDemo/WRSegmentedControl.m
@@ -0,0 +1,21 @@
+//
+//  WRSegmentedControl.m
+//  WRSegmentedControlDemo
+//
+//  Created by xianghui on 2017/5/27.
+//  Copyright © 2017年 xianghui. All rights reserved.
+//
+
+#import "WRSegmentedControl.h"
+
+@implementation WRSegmentedControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
