d878a150b8ee4aa41c6ac077e9b14d4bd52e3e0c
xianghui <xianghui_ios@163.com>
2017-05-27 10:10:16 +0800

    创建项目

---
+++ WRSegmentedControlDemo/WRSegmentedControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WRSegmentedControlDemo
+//
+//  Created by xianghui on 2017/5/27.
+//  Copyright © 2017年 xianghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WRSegmentedControlDemo/WRSegmentedControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WRSegmentedControlDemo
+//
+//  Created by xianghui on 2017/5/27.
+//  Copyright © 2017年 xianghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
